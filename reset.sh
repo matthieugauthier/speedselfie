399598b9ffbfe57910992be611152ad71660624e
Matthieu GAUTHIER <matthieu.gauthier@external.grdf.fr>
2016-06-13 22:43:47 +0200

    bug

--- reset.sh
+++ reset.sh
@@ -1,6 +1,6 @@
 rm web/uploads/*
 #php bin/console d:d:d --force --if-exists
-/usr/local/php7.0/bin/php bin/console bin/console d:s:d --force
-/usr/local/php7.0/bin/php bin/console bin/console d:s:c
-/usr/local/php7.0/bin/php bin/console bin/console import:users equipe.csv
-/usr/local/php7.0/bin/php bin/console bin/console import:questions questions.csv
+/usr/local/php7.0/bin/php bin/console d:s:d --force
+/usr/local/php7.0/bin/php bin/console d:s:c
+/usr/local/php7.0/bin/php bin/console import:users equipe.csv
+/usr/local/php7.0/bin/php bin/console import:questions questions.csv
